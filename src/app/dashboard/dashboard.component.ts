6d9cbcb88e94514e86bd16053f8603c57aa73f6b
Jalakandeswar <jalakandeswar.c.p@gmail.com>
2025-05-16 10:12:21 +0530

    dashboard changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './dashboard.component.html',  // ✅ Use external HTML file
-  styleUrls: ['./dashboard.component.scss']   // ✅ Link SCSS
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']   
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  constructor(private router: Router) {}
+
+  logout(): void {
+    
+    this.router.navigate(['/']); 
+  }
+}
